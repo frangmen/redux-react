5e95beb98a21af131d1cd6dc7b3c2a50961428c2
frangmen <frangm2702@gmail.com>
2023-07-14 17:56:31 -0600

    delete button state

--- src/components/Tasks/TaskList.js
+++ src/components/Tasks/TaskList.js
@@ -6,41 +6,64 @@ import { useState } from "react";
 
 const TaskList = ({ tasks, deleteTask }) => {
     const [editableTask, setEditableTask] = useState(null);
+    const [deleteMode, setDeleteMode] = useState(false);
+    const [deleteButtonActive, setDeleteButtonActive] = useState(false);
+
     const handleDeleteTask = (taskId) => {
         deleteTask(taskId);
+    };
+
+    const toggleDeleteMode = () => {
+        setDeleteMode(!deleteMode);
+    };
+
+    const toggleDeleteButton = () => {
+        setDeleteButtonActive(!deleteButtonActive);
+        if (!deleteButtonActive) {
+            setDeleteMode(false);
+        }
     };
 
     const handleCopyTask = (task) => {
         navigator.clipboard.writeText(task);
     };
+
     return (
-        <ul>
-            {tasks.map((task) => (
-                <li
-                    className='tasks'
-                    key={task.id}
-                    onClick={() => handleCopyTask(task.task)}>
-                    {task.task}
-                    {editableTask && editableTask.id === task.id ? (
-                        <input
-                            type='text'
-                            value={editableTask.task}
-                            onChange={(e) =>
-                                setEditableTask({
-                                    ...editableTask,
-                                    task: e.target.value,
-                                })
-                            }
-                            onBlur={() => setEditableTask(null)}
-                        />
-                    ) : (
-                        <button onClick={() => handleDeleteTask(task.id)}>
-                            Eliminar
-                        </button>
-                    )}
-                </li>
-            ))}
-        </ul>
+        <div>
+            <button onClick={toggleDeleteMode}>
+                {deleteMode ? "Desactivar Eliminar" : "Activar Eliminar"}
+            </button>
+            <ul>
+                {tasks.map((task) => (
+                    <li
+                        className='tasks'
+                        key={task.id}
+                        onClick={() => handleCopyTask(task.task)}>
+                        {task.task}
+                        {editableTask && editableTask.id === task.id ? (
+                            <input
+                                type='text'
+                                value={editableTask.task}
+                                onChange={(e) =>
+                                    setEditableTask({
+                                        ...editableTask,
+                                        task: e.target.value,
+                                    })
+                                }
+                                onBlur={() => setEditableTask(null)}
+                            />
+                        ) : (
+                            deleteMode && (
+                                <button
+                                    onClick={() => handleDeleteTask(task.id)}>
+                                    Eliminar
+                                </button>
+                            )
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
